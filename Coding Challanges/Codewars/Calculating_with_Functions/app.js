6491f11642982053681ed95ff473f2965be255e3
Ayon <mirelahi33@gmail.com>
2022-05-21 00:08:34 -0400

    Playing with js

--- Coding Challanges/Codewars/Calculating_with_Functions/app.js
+++ Coding Challanges/Codewars/Calculating_with_Functions/app.js
@@ -90,5 +90,8 @@ function times(cb) {
 function dividedBy(cb) {
   return ["/", cb];
 }
-
-console.log(three(plus(seven())));
+// Write a function that takes two strings (a and b) as arguments
+// Return the number of times a occurs in b
+function myFunction(a, b) {
+  return b.split(a).length - 1;
+}
